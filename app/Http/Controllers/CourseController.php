8f80f330ad6c73709415ae8feb750cbde05f3630
manofsteeljj <justinjoseph.sanchez@lorma.edu>
2026-02-21 00:45:02 +0800

    Structure

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'course_code' => 'required',
+            'course_name' => 'required',
+        ]);
+
+        Course::create([
+            'user_id' => auth()->id(),
+            'course_code' => $request->course_code,
+            'course_name' => $request->course_name,
+            'description' => $request->description,
+            'academic_term' => $request->academic_term,
+        ]);
+
+        return redirect()->route('courses.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Course $course)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Course $course)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Course $course)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Course $course)
+    {
+        //
+    }
+}
